6f5bac05fc4c2ad2bf69565740411c956a48cac2
Andrew Walker <awalker@ixsystems.com>
2024-09-12 08:56:07 -0600

    Expand tests

--- src/middlewared/middlewared/pytest/unit/utils/test_pam_tdb.py
+++ src/middlewared/middlewared/pytest/unit/utils/test_pam_tdb.py
@@ -13,6 +13,11 @@ EXPIRED_TS = 1
 BASE_ID = 1325
 LEGACY_ENTRY_KEY = 'rtpz6u16l42XJJGy5KMJOVfkiQH7CyitaoplXy7TqFTmY7zHqaPXuA1ob07B9bcB'
 LEGACY_ENTRY_HASH = '$pbkdf2-sha256$29000$CyGktHYOwXgvBYDQOqc05g$nK1MMvVuPGHMvUENyR01qNsaZjgGmlt3k08CRuC4aTI'
+INVALID_HASH_TYPE = '$pbkdf2-canary$29000$CyGktHYOwXgvBYDQOqc05g$nK1MMvVuPGHMvUENyR01qNsaZjgGmlt3k08CRuC4aTI'
+INVALID_SALT = '$pbkdf2-sha256$29000$CyGktHYOwXgvBYDQOqc0*g$nK1MMvVuPGHMvUENyR01qNsaZjgGmlt3k08CRuC4aTI'
+INVALID_HASH = '$pbkdf2-sha256$29000$CyGktHYOwXgvBYDQOqc05g$nK1MMvVuPGHMvUENyR01qNsaZjgGmlt3k08CRuC4a*I'
+MISSING_SALT = '$pbkdf2-sha256$29000$$nK1MMvVuPGHMvUENyR01qNsaZjgGmlt3k08CRuC4aTI'
+MISSING_HASH = '$pbkdf2-sha256$29000$CyGktHYOwXgvBYDQOqc05g$'
 PAM_DIR = '/etc/pam.d'
 PAM_FILE = 'middleware-api-key'
 PAM_AUTH_LINE = 'auth  [success=1 default=die]      pam_tdb.so debug '
@@ -41,6 +46,8 @@ def write_tdb_file(
     """
 
     keys = []
+    idx = 0
+
     for idx, hash in enumerate(hashlist):
         keys.append(user_api_key.UserApiKey(
             userhash=hash,
@@ -84,7 +91,7 @@ def make_null_tdb_entry(username: str) -> None:
     hdl = tdb.open(user_api_key.PAM_TDB_FILE)
     try:
         data = hdl.get(username.encode())
-        hdl.store(username.encode(), b'\x00\x00\x00\x00')
+        hdl.store(username.encode(), b'\x00' * 128)
     finally:
         hdl.close()
 
@@ -126,6 +133,20 @@ def fail_delay() -> None:
 def current_username() -> str:
     """ for simplicity sake we'll test against current user """
     return pwd.getpwuid(os.geteuid()).pw_name
+
+
+def test_unknown_user(current_username):
+    """
+    A user without an entry in the file should fail with appropriate error
+    and generate a fail delay
+    """
+    db_id = write_tdb_file(current_username, [LEGACY_ENTRY_HASH])
+    with pam_service(admin_user=current_username) as svc:
+        p = pam.pam()
+        with fail_delay():
+            authd = p.authenticate('canary', f'{db_id}-{LEGACY_ENTRY_KEY}', service=svc)
+            assert authd is False
+            assert p.code == pam.PAM_USER_UNKNOWN
 
 
 def test_legacy_auth_admin(current_username):
@@ -195,6 +216,17 @@ def test_new_auth(current_username):
             assert authd is False
             assert p.code == pam.PAM_AUTH_ERR
 
+def test_new_auth_truncated_password(current_username):
+    key = crypto.generate_string(string_size=64)
+    db_id = write_tdb_file(current_username, [crypto.generate_pbkdf2_512(key)])
+
+    with pam_service() as svc:
+        p = pam.pam()
+        with fail_delay():
+            authd = p.authenticate(current_username, f'{db_id}-', service=svc)
+            assert authd is False
+            assert p.code == pam.PAM_AUTH_ERR
+
 
 def test_new_auth_multi(current_username):
     """ verify that second key works with newer hash """
@@ -223,21 +255,50 @@ def test_new_auth_timeout(current_username):
     db_id = write_tdb_file(current_username, [crypto.generate_pbkdf2_512(key)], True)
     with pam_service() as svc:
         p = pam.pam()
-        # verify that using correct key succeeds
-        authd = p.authenticate(current_username, f'{db_id}-{key}', service=svc)
-        assert authd is False
-        assert p.code == pam.PAM_AUTH_ERR
+        with fail_delay():
+            authd = p.authenticate(current_username, f'{db_id}-{key}', service=svc)
+            assert authd is False
+            assert p.code == pam.PAM_AUTH_ERR
+
+
+def test_new_auth_no_keys(current_username):
+    """ verify that entries with no keys generates a pam delay as expected """
+    db_id = write_tdb_file(current_username, [])
+    with pam_service() as svc:
+        p = pam.pam()
+        with fail_delay():
+            authd = p.authenticate(current_username, f'{db_id}-', service=svc)
+            assert authd is False
+            assert p.code == pam.PAM_AUTH_ERR
 
 
 def test_unsupported_service_file_name(current_username):
     """ pam_tdb has strict check that it can't be used for other services """
     key = crypto.generate_string(string_size=64)
-    db_id = write_tdb_file(current_username, [crypto.generate_pbkdf2_512(key)], True)
+    db_id = write_tdb_file(current_username, [crypto.generate_pbkdf2_512(key)])
     with pam_service(file_name='canary') as svc:
         p = pam.pam()
         # verify that using correct key succeeds
         authd = p.authenticate(current_username, f'{db_id}-{key}', service=svc)
         assert authd is False
+        assert p.code == pam.PAM_SYSTEM_ERR
+
+
+@pytest.mark.parametrize('thehash', [
+     INVALID_HASH_TYPE,
+     INVALID_SALT,
+     INVALID_HASH,
+     MISSING_SALT,
+     MISSING_HASH,
+])
+def test_invalid_hash(current_username, thehash):
+    db_id = write_tdb_file(current_username, [thehash])
+    with pam_service(admin_user=current_username) as svc:
+        p = pam.pam()
+        # verify that using correct key succeeds
+        authd = p.authenticate(current_username, f'{db_id}-{LEGACY_ENTRY_KEY}', service=svc)
+        assert authd is False
+        assert p.code == pam.PAM_AUTH_ERR
 
 
 @pytest.mark.parametrize('fuzz_fn', [
@@ -245,7 +306,7 @@ def test_unsupported_service_file_name(current_username):
     make_tdb_garbage,
     make_null_tdb_entry,
 ])
-def test_fuzz_pam_module(current_username, fuzz_fn):
+def test_invalid_tdb_data(current_username, fuzz_fn):
     """ verify we detect garbage tdb entry and flag for reinit"""
     key = crypto.generate_string(string_size=64)
     db_id = write_tdb_file(current_username, [crypto.generate_pbkdf2_512(key)], True)
